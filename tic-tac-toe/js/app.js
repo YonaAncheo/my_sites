8ac4dbcf19f487c7319b889fa35fd4b740c8a767
Yonathan <111770436+YonaAncheo@users.noreply.github.com>
2025-06-05 23:31:14 -0400

    trying with some ways to fill board.

--- tic-tac-toe/js/app.js
+++ tic-tac-toe/js/app.js
@@ -1,5 +1,36 @@
 const board = document.getElementById("board");
+const row1 = document.getElementById('row-1');
+const row2 = document.getElementById('row-2');
+const row3 = document.getElementById('row-3');
+const winCondition = {
+  'X': ['X','X','X'],
+  'O': ['O','O','O'],
+  'row-1' : ['','',''],
+  'row-2' : ['','',''],
+  'row-3' : ['','',''],
+  'diag-1': ['','',''],
+  'diag-2': ['','','']
+};
 
 board.addEventListener('click', (event) => {
-  event.target.textContent = 'X';
+  if(event.target.nodeName === 'BUTTON'){
+    event.target.textContent = 'X';
+    noteMove(event.target);
+  }
 })
+
+function noteMove(target) {
+  let col = 0;
+  let move = target.textContent;
+  let id = target.parentElement.id;
+  if (target.id === 'col-b') {col = 1}
+  else if (target.id === 'col-c') {col = 2};
+  
+  winCondition[id][col] = move;
+}
+
+function thereAWinner(){
+  if(winCondition["row-1"] === winCondition.X || winCondition["row-2"] === winCondition.X || winCondition["row-3"] === winCondition.X ){
+    alert("PLAYER X WIN");
+  }
+}
